ae9fce4fbb99e0676c3fcd1bca1403eb2c3f6d14
JavCan <javicanella69@gmai.com>
2026-01-18 20:37:17 -0600

    Initial Commit

---
+++ Pulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pulse
+//
+//  Created by Javier Canella Ramos on 18/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Javier Canella Ramos on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
