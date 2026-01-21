04fa0a217740b2bb35b8b0ef551c0ccff44c5ce1
JavCan <javicanella69@gmai.com>
2026-01-21 14:15:50 -0600

    Commit inicial, carta de afirmaciones

---
+++ Pulse/Extensions/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  Pulse
+//
+//  Created by Javier Canella Ramos on 21/01/26.
+//
+
+import Foundation
---
+++ Pulse/ScreenHome.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenHome.swift
+//  Pulse
+//
+//  Created by Javier Canella Ramos on 18/01/26.
+//
+
+import SwiftUI
+
+struct ScreenHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenHome()
+}
